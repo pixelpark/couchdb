9855ac8732361341d42d25b0eec9998bba6ef93b
Frank Brehm <frank.brehm@pixelpark.com>
2019-09-27 13:33:14 +0200

    Changing manifests/install.pp for RedHat based servers

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,34 +2,51 @@
 
 class couchdb::install {
 
-  # Adds the repository of couchdb.
-  apt::source { 'apache_couchdb':
-    location => 'https://apache.bintray.com/couchdb-deb',
-    repos    => 'main',
-    include  => {
-      'deb' => true,
-    },
-    before   => Apt::Key['couchdb_key'],
+  $package_ensure = $couchdb::package_ensure
+
+  if $::osfamily == 'RedHat' {
+
+    package { 'couchdb':
+      ensure => $package_ensure,
+    }
+
+  }
+  elsif $::osfamily == 'Debian' {
+
+    # Adds the repository of couchdb.
+    apt::source { 'apache_couchdb':
+      location => 'https://apache.bintray.com/couchdb-deb',
+      repos    => 'main',
+      include  => {
+        'deb' => true,
+      },
+      before   => Apt::Key['couchdb_key'],
+    }
+
+    # Adds the key for the repository.
+    apt::key { 'couchdb_key':
+      id     => '8756C4F765C9AC3CB6B85D62379CE192D401AB61',
+      server => 'pgp.mit.edu',
+      before => Class['apt::update'],
+    }
+
+    # Installs curl.
+    package { 'curl':
+      ensure => installed,
+    }
+
+    # Updates all repositories.
+    Class['apt::update'] -> Package <| provider == 'apt' |>
+
+    # Installs the couchdb package.
+    package { 'couchdb':
+      ensure  => installed,
+      require => Class['apt::update'],
+    }
+
+  }
+  else {
+    fail("Unsupported operating system '$::osfamily'.")
   }
 
-  # Adds the key for the repository.
-  apt::key { 'couchdb_key':
-    id     => '8756C4F765C9AC3CB6B85D62379CE192D401AB61',
-    server => 'pgp.mit.edu',
-    before => Class['apt::update'],
-  }
-
-  # Installs curl.
-  package { 'curl':
-    ensure => installed,
-  }
-
-  # Updates all repositories.
-  Class['apt::update'] -> Package <| provider == 'apt' |>
-
-  # Installs the couchdb package.
-  package { 'couchdb':
-    ensure  => installed,
-    require => Class['apt::update'],
-  }
 }
